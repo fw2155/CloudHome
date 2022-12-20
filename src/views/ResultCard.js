597b89f286b16c42afaa963f3c3d7adce5de30b3
Fangyi <fw2155@nyu.edu>
2022-12-20 04:42:05 -0500

    general search works

---
+++ src/views/ResultCard.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+const imageList = [
+    'https://media.istockphoto.com/id/1388026461/photo/apartment-buildings-in-a-residential-area.jpg?b=1&s=170667a&w=0&k=20&c=qXuT2g8XBsOlEcFNGltso_GApGFZEV0FjR2S44VuFK4=',
+    'https://thumbs.cityrealty.com/assets/smart/736x/webp/1/16/1655f4e3904fb79cb987ab7755d2b3f4b8f37f88/1-city-point.jpg',
+    'https://thumbs.cityrealty.com/assets/smart/736x/webp/2/22/22ca6350cd0dcc474556e1e06bd8144482a45135/15-central-park-west-01.jpg',
+    'https://img.zumpercdn.com/486165561/1280x960',
+    'https://www.relatedrentals.com/sites/default/files/2022-10/Westminster_16H_3.jpg',
+    'https://images1.apartments.com/i2/F7_APKUtPtKPLll5pNYij_Z7YqW0p6swHTh4mEXsqbI/117/95-wall-new-york-ny-building-photo.jpg',
+    'https://cdn.vox-cdn.com/thumbor/KUvQg5uiYBTU0txCmnv3DTu1nL4=/0x0:1000x667/1200x800/filters:focal(420x254:580x414)/cdn.vox-cdn.com/uploads/chorus_image/image/50994531/90sfstudio.0.jpg',
+    'https://res.cloudinary.com/apartmentlist/image/upload/f_auto,q_auto,t_web-base/0ae853f54511b8f343168aae5d86a326.jpg',
+    'https://www.decorilla.com/online-decorating/wp-content/uploads/2022/03/New-York-apartment-style-concept-by-Decorilla.jpg',
+    'https://wp-tid.zillowstatic.com/bedrock/app/uploads/sites/26/nyc-apartments-for-1m-west-village-e3b3e3.jpeg',
+    'https://images1.apartments.com/i2/8t5pitCqc_akYi1v0sgsGGWPex7l6cgw0Frlk7xhcmE/117/the-ritz-plaza-new-york-ny-building-photo.jpg',
+    'https://images1.apartments.com/i2/QXrOPFeW1b-em5PR_I3yNdDgtvlhO5fvdO_Mvrwl9iU/117/park-towers-south-new-york-ny-building-photo.jpg',
+    'https://img.zumpercdn.com/434939807/1280x960',
+    'https://cdngeneral.rentcafe.com/dmslivecafe/3/1112048/HawkinsWay-97ColumbiaHeights-3925.jpg?width=380',
+    'https://images1.apartments.com/i2/c4ZU6MfpZ0HHHw2TYSLsSUNREKiCdB4waAEcNnAqs98/117/517-w-113th-st-new-york-ny-building-photo.jpg',
+    'https://photos.zillowstatic.com/fp/00ca20c0f040f67e66bfc6738275e5fa-p_e.jpg',
+    'https://images1.apartments.com/i2/47YF1asg2uFn7Gadwamv0VMhCQmkAmWEGiDGCNIM37k/117/the-cole-by-stonehenge-new-york-ny-interior-photo.jpg',
+    'https://photos.renthop.com/p/c/420x300/65252945_68e70135d1d0455e808b9a7ec60f70d4.jpg',
+    'https://photos.zillowstatic.com/fp/3b3f51fa2a0c860bd84541b81d1e0a96-se_large_800_400.jpg',
+    'https://photos.renthop.com/p/c/420x300/65900238_7ceeae776287d8c733be574e98f64e65.jpg',
+    'https://images1.forrent.com/i2/VW-X3p9vcQ2YBd8WiTj9lOkF8y9XUK6O0cNEGukjwvU/117/image.jpg',
+
+]
+
+export default function ResultCard({ address, price, fullAddress, bedroom }) {
+    const [expanded, setExpanded] = React.useState(false);
+    const [iconColor, setIconColor] = React.useState('inherit');
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    const handleFavClick = () => {
+        setIconColor(iconColor === 'inherit' ? 'secondary' : 'inherit');
+    };
+    const index = Math.floor(Math.random() * imageList.length);
+    const imageUrl = imageList[index];
+
+
+    return (
+        <Card sx={{ width: 345, height: 430 }}>
+            <CardHeader
+                // avatar={
+                //     <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                //         R
+                //     </Avatar>
+                // }
+                // action={
+                //     <IconButton aria-label="settings">
+                //         <MoreVertIcon />
+                //     </IconButton>
+                // }
+                title={address}
+                subheader={`$${price}`}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={imageUrl}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {fullAddress}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Bedroom: {bedroom}
+                </Typography>
+            </CardContent>
+
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites" onClick={handleFavClick}>
+                    <FavoriteIcon color={iconColor} />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                {/* <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore> */}
+            </CardActions>
+            {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                        aside for 10 minutes.
+                    </Typography>
+                    <Typography paragraph>
+                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+                        occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+                        large plate and set aside, leaving chicken and chorizo in the pan. Add
+                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+                        stirring often until thickened and fragrant, about 10 minutes. Add
+                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                    </Typography>
+                    <Typography paragraph>
+                        Add rice and stir very gently to distribute. Top with artichokes and
+                        peppers, and cook without stirring, until most of the liquid is absorbed,
+                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+                        mussels, tucking them down into the rice, and cook again without
+                        stirring, until mussels have opened and rice is just tender, 5 to 7
+                        minutes more. (Discard any mussels that don&apos;t open.)
+                    </Typography>
+                    <Typography>
+                        Set aside off of the heat to let rest for 10 minutes, and then serve.
+                    </Typography>
+                </CardContent>
+            </Collapse> */}
+        </Card>
+    );
+}
--- src/views/SearchApts.js
+++ src/views/SearchApts.js
@@ -4,10 +4,29 @@ import { inputAdornmentClasses } from "@mui/material";
 import { aptService } from "../services/aptService";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Box from '@mui/material/Box';
+import ResultCard from "./ResultCard";
+
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export default function SearchApts() {
     const location = useLocation();
     // console.log(location.pathname)
+    //fetch passed value from previous page
     const textInput = location.search.slice(3, location.search.length) //"1%20Dekalb%20Ave"
     const decodedAddress = decodeURIComponent(textInput); //1 Dekalb
     const searchType = location.state.searchType
@@ -15,51 +34,113 @@ export default function SearchApts() {
     console.log('searchType', searchType)
 
     const [urls, setUrls] = React.useState([]);
-
+    const [obj, setObj] = React.useState([]);
     useEffect(function () {
         getSearchResult(textInput);
     }, [])
 
-    const config = {
-        headers: {
-            // 'isGeneral': searchType,
-            'isGeneral': 'general',
-            // 'Accept': '*/*',
-            // 'Content-Type': 'application/json',
-            // 'X-Api-Key': 'CL93pjmrta9aRzTAxqzne9PLZ1zUaXwL2GRjLQCA',
-
-        },
-    };
     const getSearchResult = (decodedAddress) => {
         // call API
         console.log('hi', decodedAddress)
-        aptService.getApt(decodedAddress, config)
+        aptService.getApt(decodedAddress, {
+            headers: {
+                'isgeneral': 'general'
+            }
+        })
             .then(function (response) {
                 console.log(response)
-                // setUrls(response.data.results)
+                setObj(response.data.results)
+                console.log(obj)
             })
             .catch(function (error) {
                 console.log(error)
             })
         console.log('check')
     }
-    // console.log(urls)
 
-    // const urls = ['https://photo-fw2155-zz3523.s3.amazonaws.com/dragon-posing-with-fire.png', 'https://photo-fw2155-zz3523.s3.amazonaws.com/006.png']
+
+    const numColumns = 3;
+    const numRows = Math.ceil(obj.length / numColumns);
+
+
 
     return (
         // <h1>{location.search}</h1>
-        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-            {urls.map((item) => (
-                <ImageListItem key={item}>
-                    <img
-                        src={`${item}?w=164&h=164&fit=crop&auto=format`}
-                        // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                        // alt={item.title}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignContent: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: "100vh",
+                backgroundColor: '#e2deee',
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+            }}
+        >
+            <div>
+                {[...Array(numRows)].map((_, rowIndex) => (
+                    <Box key={rowIndex} display="flex">
+                        {obj.slice(rowIndex * numColumns, (rowIndex + 1) * numColumns).map(result => (
+                            <Box key={result.Address} flex={1} m={1}>
+                                <ResultCard
+                                    address={result.Address}
+                                    price={result.Price}
+                                    fullAddress={result.FullAddress}
+                                    bedroom={result.Bedroom}
+                                />
+                            </Box>
+                        ))}
+                    </Box>
+                ))}
+            </div>
+
+            {/* <div>
+                {obj.map(result => (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignContent: 'center',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            minHeight: "100vh",
+                            backgroundColor: '#e2deee',
+                            backgroundSize: "cover",
+                            backgroundPosition: "center center",
+                        }}
+                    >
+                        <ResultCard
+                            // key={result.address}
+                            // address={result.address}
+                            // price={result.price}
+                            // fullAddress={result.fullAddress}
+                            // bedroom={result.bedroom}
+                            address={result.Address}
+                            price={result.Price}
+                            fullAddress={result.FullAddress}
+                            bedroom={result.Bedroom}
+                        />
+                    </Box>
+                ))}
+            </div> */}
+
+
+
+
+            {/* <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                {urls.map((item) => (
+                    <ImageListItem key={item}>
+                        <img
+                            src={`${item}?w=164&h=164&fit=crop&auto=format`}
+                            // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            // alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList> */}
+        </Box >
     );
 };
