e00ca365db9187f122389614da24d166d5586ccf
Fangyi <fw2155@nyu.edu>
2023-02-23 23:44:21 -0500

    update bot

--- src/components/KommunicateChat.js
+++ src/components/KommunicateChat.js
@@ -7,7 +7,7 @@ class KommunicateChat extends Component {
     componentDidMount() {
         (function (d, m) {
             var kommunicateSettings =
-                { "appId": "3f5f61585517395599541eb82a09239f8", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+                { "appId": "xxxxx", "popupWidget": true, "automaticChatOpenOnNavigation": true };
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
